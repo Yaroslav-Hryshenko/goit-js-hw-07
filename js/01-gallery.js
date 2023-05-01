e8ea2232e1921cd05a38d9de6cb0e0f077ab12d9
Yaroslav-Hryshenko <yaroslav.cook81@gmail.com>
2023-05-01 14:45:23 +0300

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
+// Change code below this line
 console.log(galleryItems);
+
+const galleryConteiner = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) => ` <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+         <img
+         class="gallery__image"
+         src="${item.preview}"
+         data-source="${item.original}"
+        alt="${item.description}"
+        />
+         </a>
+</li>`
+    )
+    .join("");
+}
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryConteiner.innerHTML = galleryMarkup;
+galleryConteiner.addEventListener("click", onImageClik);
+
+function onImageClik(evt) {
+  blockStandartAction(evt);
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+   
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+   
+`);
+ instance.show();
+  
+
+  galleryConteiner.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+    
+    
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
+
